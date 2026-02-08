7a008e3085dcc813d2a126a1aa933fda2fbd7494
sven-hash <>
2026-02-08 07:10:27 +0100

    add: burn

--- contracts/test/integration/token.test.ts
+++ contracts/test/integration/token.test.ts
@@ -1,4 +1,4 @@
-import { web3, DUST_AMOUNT, ONE_ALPH, prettifyAttoAlphAmount, number256ToBigint, number256ToNumber, NULL_CONTRACT_ADDRESS, sleep, ALPH_TOKEN_ID } from '@alephium/web3'
+import { web3, DUST_AMOUNT, ONE_ALPH, prettifyAttoAlphAmount, number256ToBigint, number256ToNumber, NULL_CONTRACT_ADDRESS, sleep, ALPH_TOKEN_ID, MINIMAL_CONTRACT_DEPOSIT } from '@alephium/web3'
 import { expectAssertionError, mintToken, testNodeWallet } from '@alephium/web3-test'
 import { deployToDevnet } from '@alephium/cli'
 import { ChainReaction } from '../../artifacts/ts'
@@ -595,6 +595,200 @@ describe('integration tests', () => {
     expect(state.asset.alphAmount).toEqual(2n*435780500000000000n-1n * 10n ** 17n)
     expect(state.fields.pot).toEqual(0n)
     expect(state.fields.boostAmount).toEqual(0n)
+    expect(state.asset.tokens).toEqual([])
+
+
+  }, 20000)
+
+      it('start game with players playing custom token, check burn', async () => {
+    const signer = await testNodeWallet()
+
+    const tokenTest = await mintToken((await signer.getSelectedAccount()).address,1000000n)
+    await transferTokenTo(minters[0].address,tokenTest.tokenId, 100n)
+
+    let now = BigInt(Date.now())
+    const deployed = await ChainReaction.deploy(signer, {
+      initialFields: {
+        baseEntry: 0n,
+        chainId: 0n,
+        currentEntry: 0n,
+        endTimestamp: 0n,
+        isActive: false,
+        lastEntryTimestamp: 0n,
+        durationMs: 0n,
+        lastPlayer: NULL_CONTRACT_ADDRESS,
+        multiplierBps: 1000n, // <-- customize per test
+        playerCount: 0n,
+        pot: 0n,
+        durationDecreaseMs: 500n,
+        minDuration: 500n,
+        tokenId: tokenTest.tokenId,
+        boostAmount: 0n,
+        burnBps: 0n,
+        burnedAmount: 0n
+      }
+    })
+
+    const game = deployed.contractInstance
+    expect(game).toBeDefined()
+
+    if (!game) {
+      throw new Error('Game is undefined')
+    }
+    const initialState = await game.fetchState()
+    const multiplierBps = initialState.fields.multiplierBps
+    expect(multiplierBps).toEqual(1000n)
+    expect(initialState.fields.isActive).toEqual(false)
+
+    await game.transact.startChain({
+      args: {
+        payment: 1n,
+        durationGameMs: 500n,
+        multiplierGameBps: 1000n,
+        tokenIdGame: tokenTest.tokenId,
+        burnRate: 500n
+      },
+      signer: minters[0],
+      attoAlphAmount: DUST_AMOUNT,
+      tokens: [{
+        id: tokenTest.tokenId,
+        amount: 1n
+      }]
+    })
+
+
+    let state = await game.fetchState()
+
+    expect(state.fields.pot).toEqual(1n)
+    expect(state.asset.alphAmount).toEqual(MINIMAL_CONTRACT_DEPOSIT)
+
+
+    let nextPayment = (await game.view.getNextEntryPrice()).returns
+    expect(nextPayment).toEqual(1n)
+
+    for (let index = 1; index < 6; index++) {
+      await transferTokenTo(minters[index].address, tokenTest.tokenId,5n)
+
+      const payment = (await game.view.getNextEntryPrice()).returns
+      const canEnd = (await game.view.canEnd()).returns
+
+      expect(canEnd).toBe(false)
+      state = await game.fetchState()
+
+      now = BigInt(Date.now())
+      expect(state.fields.endTimestamp).toBeGreaterThan(now)
+
+      await game.transact.joinChain({
+        args: {
+          payment: payment
+        },
+        signer: minters[index],
+        attoAlphAmount:  DUST_AMOUNT,
+        tokens: [{
+        id: tokenTest.tokenId,
+        amount: payment
+      }]
+      })
+
+    }
+
+    await sleep(1000)
+
+    state = await game.fetchState()
+    now = BigInt(Date.now())
+    expect(state.fields.endTimestamp).toBeLessThanOrEqual(now)
+    expect((await game.view.canEnd()).returns).toBe(true)
+    expect(state.fields.burnedAmount).toBeGreaterThan(0n)
+
+
+    await game.transact.endChain({
+      signer: minters[0],
+      attoAlphAmount: DUST_AMOUNT
+    })
+
+    state = await game.fetchState()
+    console.log(prettifyAttoAlphAmount(state.asset.alphAmount))
+    expect(state.asset.alphAmount).toEqual(MINIMAL_CONTRACT_DEPOSIT)
+    expect(state.fields.pot).toEqual(0n)
+    expect(state.fields.boostAmount).toEqual(0n)
+    expect(state.fields.burnedAmount).toEqual(0n)
+    expect(state.asset.tokens).toEqual([])
+
+
+    await game.transact.startChain({
+      args: {
+        payment: 1n,
+        durationGameMs: 500n,
+        multiplierGameBps: 1000n,
+        tokenIdGame: tokenTest.tokenId,
+        burnRate: 500n
+      },
+      signer: minters[0],
+      attoAlphAmount: DUST_AMOUNT,
+      tokens: [{
+        id: tokenTest.tokenId,
+        amount: 1n
+      }]
+    })
+
+
+    state = await game.fetchState()
+
+    expect(state.fields.pot).toEqual(1n)
+    expect(state.asset.alphAmount).toEqual(MINIMAL_CONTRACT_DEPOSIT)
+
+
+     nextPayment = (await game.view.getNextEntryPrice()).returns
+    expect(nextPayment).toEqual(1n)
+
+    for (let index = 1; index < 6; index++) {
+
+      const payment = (await game.view.getNextEntryPrice()).returns
+      const canEnd = (await game.view.canEnd()).returns
+
+      expect(canEnd).toBe(false)
+      state = await game.fetchState()
+
+      now = BigInt(Date.now())
+      expect(state.fields.endTimestamp).toBeGreaterThan(now)
+
+      await game.transact.joinChain({
+        args: {
+          payment: payment
+        },
+        signer: minters[index],
+        attoAlphAmount: DUST_AMOUNT,
+        tokens: [{
+          id: tokenTest.tokenId,
+          amount: payment
+        }]
+      })
+
+    }
+
+
+    await sleep(1000)
+
+    state = await game.fetchState()
+    now = BigInt(Date.now())
+    expect(state.fields.endTimestamp).toBeLessThanOrEqual(now)
+    expect(state.fields.burnedAmount).toBe(0n)
+
+    expect((await game.view.canEnd()).returns).toBe(true)
+  
+
+    await game.transact.endChain({
+      signer: minters[0],
+      attoAlphAmount: DUST_AMOUNT
+    })
+
+
+    state = await game.fetchState()
+    console.log(prettifyAttoAlphAmount(state.asset.alphAmount))
+    expect(state.asset.alphAmount).toEqual(1n * 10n ** 17n)
+    expect(state.fields.pot).toEqual(0n)
+    expect(state.fields.boostAmount).toEqual(0n)
+    expect(state.fields.burnedAmount).toEqual(0n)
     expect(state.asset.tokens).toEqual([])
 
 
